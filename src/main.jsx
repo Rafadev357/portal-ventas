485c432bee87316318588790e42db11594e23721
Rafa <xanas_@hotmail.com>
2025-07-22 10:48:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { CarritoProvider } from './components/context/CarritoContext.jsx'
