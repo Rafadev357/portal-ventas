aa706f272b8bca6ba48ef0d09c10e1a270fef119
Rafa <xanas_@hotmail.com>
2025-07-22 10:40:49 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CarritoProvider } from './components/context/CarritoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <CarritoProvider>
+      <App />
+    </CarritoProvider>
+  </React.StrictMode>
 )
