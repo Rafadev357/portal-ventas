1ced103fe57dad8f31a9c5f6e67033004d0a5845
Rafa <xanas_@hotmail.com>
2025-07-25 17:35:42 +0200

    Update files

--- src/components/layout/PasarelaPago.jsx
+++ src/components/layout/PasarelaPago.jsx
@@ -5,40 +5,42 @@ export const PasarelaPago = () => {
     const { carrito } = useCarrito();
     const { precioTotal } = useCarrito();
   return (
-    <div>
-        <h2>Danos tus datos para la compra:</h2>
-        <form action="">
-            <label htmlFor="nombre">
-                Nombre:
-            <input type="text" id="nombre" name="nombre" required />
-            </label>
-            <label htmlFor="apellidos">
-                Apellidos:
-            <input type="text" id="apellidos" name="apellidos" required />
-            </label>
-            <label htmlFor="direccion">
-                Dirección:
-            <input type="text" id="direccion" name="direccion" required />
-            </label>
-            <label htmlFor="telefono">
-                Teléfono:
-            <input type="tel" id="telefono" name="telefono" required />
-            </label>
-            <label htmlFor="email">
-                Email:
-            <input type="email" id="email" name="email" required />
-            </label>
-            <label htmlFor="metodo">
-                Método de pago:
-                <select name="metodo" id="metodo" required>
-                    <option value="tarjeta">Tarjeta de crédito</option>
-                    <option value="paypal">PayPal</option>
-                    <option value="transferencia">Transferencia bancaria</option>
-                </select>
-            </label>
-            <button>confirmar compra</button>
-        </form>
-        <div>
+    <div className='layout__pasarela-pago'>
+        <div className='layout__pasarela-pago__formulario'>
+            <h2>Danos tus datos para la compra:</h2>
+            <form action="">
+                <label htmlFor="nombre">
+                    Nombre:
+                <input type="text" id="nombre" name="nombre" required />
+                </label>
+                <label htmlFor="apellidos">
+                    Apellidos:
+                <input type="text" id="apellidos" name="apellidos" required />
+                </label>
+                <label htmlFor="direccion">
+                    Dirección:
+                <input type="text" id="direccion" name="direccion" required />
+                </label>
+                <label htmlFor="telefono">
+                    Teléfono:
+                <input type="tel" id="telefono" name="telefono" required />
+                </label>
+                <label htmlFor="email">
+                    Email:
+                <input type="email" id="email" name="email" required />
+                </label>
+                <label htmlFor="metodo">
+                    Método de pago:
+                    <select name="metodo" id="metodo" required>
+                        <option value="tarjeta">Tarjeta de crédito</option>
+                        <option value="paypal">PayPal</option>
+                        <option value="transferencia">Transferencia bancaria</option>
+                    </select>
+                </label>
+                <button>confirmar compra</button>
+            </form>
+        </div>
+        <div className='layout__pasarela-pago__resumen'>
             <h3>Resumen de la compra:</h3>
             <ul>
                 {carrito.map((producto, index)=>(
